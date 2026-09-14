2dd956f510f61dd6a77854874e21535e94c54169
nhffbs <nhffbs@cesar.org.br>
2026-09-14 12:29:21 -0300

    chore: scaffold Spring Boot project with Postgres via Docker Compose

---
+++ src/main/java/com/yarnstash/YarnstashApplication.java
@@ -0,0 +1,13 @@
+package com.yarnstash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YarnstashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YarnstashApplication.class, args);
+	}
+
+}
